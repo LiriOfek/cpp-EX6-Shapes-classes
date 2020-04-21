719bcfcd09a9a6ec30ff2ca8a54ac10c93848591
LiriOfek <46228594+LiriOfek@users.noreply.github.com>
2020-04-21 13:52:47 +0300

    add Circle class and functions of circle

---
+++ Ex6-Shapes_classes/Circle.cpp
@@ -0,0 +1,22 @@
+/********************************************************\
+File Name: Circle.cpp
+Author: Liri (21/4/2020)
+Purpose: This file contain class of Circle, that inheritor from Shape,
+with its functions and parameter
+\********************************************************/
+
+#include "Circle.h"
+
+Circle::Circle(double circle_radius)
+{
+	radius = circle_radius;
+}
+
+double Circle::getArea()
+{
+	return radius * radius * M_PI;
+}
+
+double Circle::getPerimeter() {
+	return TWO * M_PI * radius;
+}
--- Ex6-Shapes_classes/Circle.h
+++ Ex6-Shapes_classes/Circle.h
@@ -4,3 +4,42 @@ Author: Liri (21/4/2020)
 Purpose: This header file contain class of Circle, that inheritor from Shape,
 			with its functions and parameters
 \********************************************************/
+
+#pragma once
+
+#define _USE_MATH_DEFINES
+#include <cmath>
+#include "Shape.h"
+
+const int TWO = 2;
+
+/*class Circle, that inheritor from Shape, and contain functions that
+get area and get perimeter of the circle*/
+class Circle : public Shape {
+public:
+	Circle(double circle_radius);
+	/**
+	* @brief  create Circle and set its radius
+	* @param  IN double circle_radius - the circle radius
+	* @return this function has no return value
+	* @note   create circle and set its radius
+	* @author  Liri
+	*/
+	double getArea() override;
+	/**
+	* @brief  calculate area of circle
+	* @param  OUT double area of the circle
+	* @return the area of the circle
+	* @author  Liri
+	*/
+	double getPerimeter() override;
+	/**
+	* @brief  calculate perimeter of circle
+	* @param  OUT double perimeter of the circle
+	* @return the perimeter of the circle
+	* @author  Liri
+	*/
+
+private:
+	double radius;
+};
