6fe7c89987165b50e4a7d7ccc8798c3e8a6cd291
LiriOfek <46228594+LiriOfek@users.noreply.github.com>
2020-04-21 14:01:05 +0300

    add header file of triangle and documentation

---
+++ Ex6-Shapes_classes/Triangle.h
@@ -0,0 +1,6 @@
+/********************************************************\
+File Name: Tringle.h
+Author: Liri (21/4/2020)
+Purpose: This header file contain class of Triangle, that inheritor from Shape,
+			with its functions and parameters
+\********************************************************/
