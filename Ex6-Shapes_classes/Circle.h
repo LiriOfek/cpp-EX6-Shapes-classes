c53c3e9468941d17a5ed940e63e57e3dca210879
LiriOfek <46228594+LiriOfek@users.noreply.github.com>
2020-04-21 13:51:15 +0300

    add header file of Circle with documentation

---
+++ Ex6-Shapes_classes/Circle.h
@@ -0,0 +1,6 @@
+/********************************************************\
+File Name: Circle.h
+Author: Liri (21/4/2020)
+Purpose: This header file contain class of Circle, that inheritor from Shape,
+			with its functions and parameters
+\********************************************************/
