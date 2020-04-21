2db7976dec4efe0c09c25783b1844ad8e5ef22c9
LiriOfek <46228594+LiriOfek@users.noreply.github.com>
2020-04-21 13:49:09 +0300

    add header file of Shape with documentation

---
+++ Ex6-Shapes_classes/Shape.h
@@ -0,0 +1,6 @@
+/********************************************************\
+File Name: Shape.h
+Author: Liri (21/4/2020)
+Purpose: This header file contain class of Shape, with its
+			functions and parameter
+\********************************************************/
