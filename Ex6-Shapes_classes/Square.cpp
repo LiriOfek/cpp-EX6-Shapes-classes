2cdbfb563f1294c9b19e4f4300b4a1115b327414
LiriOfek <46228594+LiriOfek@users.noreply.github.com>
2020-04-21 13:57:22 +0300

    add Square class with its functions

---
+++ Ex6-Shapes_classes/Square.cpp
@@ -0,0 +1,23 @@
+/********************************************************\
+File Name: Square.cpp
+Author: Liri (21/4/2020)
+Purpose: This file contain class of Square, that inheritor from Shape,
+		with its functions and parameter
+\********************************************************/
+
+#include "Square.h"
+
+Square::Square(double square_length)
+{
+	length = square_length;
+}
+
+double Square::getArea()
+{
+	return length * length;
+}
+
+double Square::getPerimeter()
+{
+	return FOUR * length;
+}
--- Ex6-Shapes_classes/Square.h
+++ Ex6-Shapes_classes/Square.h
@@ -1,6 +1,42 @@
 /********************************************************\
-File Name: Shape.h
+File Name: Square.h
 Author: Liri (21/4/2020)
-Purpose: This header file contain class of Shape, with its
-			functions and parameter
+Purpose: This header file contain class of Square, that inheritor from Shape,
+			with its functions and parameters
 \********************************************************/
+
+#pragma once
+#include "Shape.h"
+
+const int FOUR = 4;
+
+/*class Square, that inheritor from Shape, and contain functions that
+get area and get perimeter of the square*/
+class Square : public Shape {
+public:
+	Square(double square_length);
+	/**
+	* @brief  create Square and set its length
+	* @param  IN double square_length - the square length
+	* @return this function has no return value
+	* @note   create square and set its length
+	* @author  Liri
+	*/
+	double getArea() override;
+	/**
+	* @brief  calculate area of square
+	* @param  OUT double area of the square
+	* @return the area of the square
+	* @author  Liri
+	*/
+	double getPerimeter() override;
+	/**
+	* @brief  calculate perimeter of square
+	* @param  OUT double perimeter of the square
+	* @return the perimeter of the square
+	* @author  Liri
+	*/
+
+private:
+	double length;
+};
